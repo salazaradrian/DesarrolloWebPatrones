011f9c7a6cb6024640174ebebf5d4fab7352206b
salazaradrian <87552347+salazaradrian@users.noreply.github.com>
2025-10-07 21:09:53 -0600

    Leccion03

--- TiendaTech/src/main/java/tiendaTech/com/ProjectConfig.java
+++ TiendaTech/src/main/java/tiendaTech/com/ProjectConfig.java
@@ -1,10 +1,18 @@
 package tiendaTech.com;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -68,6 +76,20 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+    @Value("${firebase.json.path}")
+    private String jsonPath;
+ 
+    @Value("${firebase.json.file}")
+    private String jsonFile;
+ 
+    @Bean
+    public Storage storage() throws IOException {
+        ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
+        try (InputStream inputStream = resource.getInputStream()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        }
     }
 
 }
--- TiendaTech/src/main/java/tiendaTech/services/CategoriaServices.java
+++ TiendaTech/src/main/java/tiendaTech/services/CategoriaServices.java
@@ -6,12 +6,15 @@
 package tiendaTech.com.services;
 
 
-import tiendaTech.com.domain.Categoria;
-import tiendaTech.com.repository.CategoriaRepository;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
  
 @Service
 public class CategoriaServices {
@@ -25,5 +28,42 @@ public class CategoriaServices {
         }
         return categoriaRepository.findAll();
     }
-
+    @Transactional(readOnly = true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
+    }
+ 
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+ 
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (!imagenFile.isEmpty()) { //Si no está vacío... pasaron una imagen...            
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile, "categoria",
+                        categoria.getIdCategoria());
+                categoria.setRutaImagen(rutaImagen);
+                categoriaRepository.save(categoria);
+            } catch (IOException e) {
+ 
+            }
+        }
+    }
+ 
+    @Transactional
+    public void delete(Integer idCategoria) {
+        // Verifica si la categoría existe antes de intentar eliminarlo
+        if (!categoriaRepository.existsById(idCategoria)) {
+            // Lanza una excepción para indicar que el usuario no fue encontrado
+            throw new IllegalArgumentException("La categoría con ID " + idCategoria + " no existe.");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            // Lanza una nueva excepción para encapsular el problema de integridad de datos
+            throw new IllegalStateException("No se puede eliminar la categoria. Tiene datos asociados.", e);
+        }
+    }
 }
